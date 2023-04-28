b6776d49a7885d818dda6f16e851b8b0effac394
Rajsuryan Singh <rajsuryan.singh@gmail.com>
2023-04-28 17:38:18 +0530

    add videos to GUI

--- data/collect_data_GUI.py
+++ data/collect_data_GUI.py
@@ -3,8 +3,10 @@ import numpy as np
 import time
 import os
 import tkinter as tk
+import threading
 from threading import Lock
 import platform
+from utils import EmgCollector, GIFViewer
 
 if platform.system() == "Windows":
     sdk_path = 'C:\\myo-sdk-win-0.9.0\\'
@@ -12,67 +14,20 @@ elif platform.system() == "Darwin":
     sdk_path = os.path.abspath('SDK/myo-sdk-mac-0.9.0')
 
 
-rec_duration = 6
+rec_duration = 4
+gesture_pairs = {"Open-Close Hand":["OpenHand", "Neutral","ClosedHand"],
+                "Thumb Abduction-Adduction":["ThumbAbduction", "Neutral", "ThumbAdduction"],
+                "Wrist Pronation-Supination":["WristPronation", "Neutral", "WristSupination"]}
+gif_viewer = None
+start_button = None
+gif_label = None
+discard_button = None
 
-class EmgCollector(myo.DeviceListener):
-    def __init__(self, start, name, forearm_circumference, gesture, gesture_index, root):
-        self.name = name
-        self.start = start
-        self.forearm_circumference = forearm_circumference
-        self.gesture = gesture
-        self.gesture_index = gesture_index
-        self.root = root
-        self.emg_data = np.zeros((1, 8))
-        self.lock = Lock()
-        self.is_recording = False
-        self.countdown_label = tk.Label(root, font=("Helvetica", 15), pady=40)
-        self.countdown_label.pack()
+def collect_emg_data(name, forearm_circumference, gesture):  
+    global gesture_index  
+    if gif_viewer is not None:
+        gif_viewer.destroy()
 
-    def on_connected(self, event):
-        event.device.stream_emg(True)
-
-    def on_emg(self, event):
-        if self.is_recording:
-            self.emg_data = np.concatenate((self.emg_data, np.reshape(np.array(event.emg), (1, 8))), axis=0)
-
-    def start_recording(self):
-        # Start countdown timer
-        countdown_secs = 5
-        for i in range(countdown_secs):
-            self.countdown_label.config(text=f"Recording {self.gesture} in {countdown_secs-i} seconds...")
-            self.root.update()
-            time.sleep(1)
-
-        # Start recording
-        self.is_recording = True
-        self.emg_data = np.zeros((1, 8))
-        self.countdown_label.config(text=f"Recording {self.gesture}...")
-        self.root.update()
-
-    def stop_recording(self):
-        # Stop recording
-        self.is_recording = False
-        self.countdown_label.config(text=f"You may relax! Saving {self.gesture} data...")
-        self.root.update()
-        time.sleep(2)
-
-        # Save EMG data to file
-        if not os.path.isdir(f"data/{self.name}"):
-            os.mkdir(f"data/{self.name}")
-
-        emg_data_array = np.array(self.emg_data)
-        filename = f"data/{self.name}/{self.gesture}_{self.gesture_index}.npy"
-        np.save(filename, emg_data_array)
-        
-        # Save forearm size 
-        filename = f"data/{self.name}/forearm_circumference.npy"
-        np.save(filename, np.array(self.forearm_circumference))
-        
-        # Increment gesture index
-        self.countdown_label.destroy()
-        self.root.update()
-
-def collect_emg_data(name, forearm_circumference, gesture, gesture_index):
     # Create participant directory if it doesn't exist
     if not os.path.exists(name):
         os.makedirs(name)
@@ -80,21 +35,102 @@ def collect_emg_data(name, forearm_circumference, gesture, gesture_index):
     # Initialize MYO and start recording
     myo.init(sdk_path=sdk_path)
     hub = myo.Hub()
-    for gesture_index in range(3):
-        start = time.time()
-        listener = EmgCollector(start, name, forearm_circumference, gesture, gesture_index, root)
-        listener.start_recording()
-        hub.run(listener, rec_duration * 1000)
-        listener.stop_recording()
+
+    gesture_index = 0
+    while gesture_index < 5:
+        if gesture in gesture_pairs:
+            for idx, gest in enumerate(gesture_pairs[gesture]):
+                countdown = 5 if idx == 0 else 3
+                start = time.time()
+                listener = EmgCollector(start, name, forearm_circumference, gest, gesture_index, root)
+                listener.start_recording(countdown)
+                hub.run(listener, rec_duration * 1000)
+                listener.stop_recording()
+        else:
+            start = time.time()
+            listener = EmgCollector(start, name, forearm_circumference, gesture, gesture_index, root)
+            listener.start_recording()
+            hub.run(listener, rec_duration * 1000)
+            listener.stop_recording()
         
+        gesture_index+=1
+    reset_GUI(root)
+
+def decrease_by_one(x):
+    x=-1
+
+def clean_gif(gif_viewer, gif_label, next_button):
+    gif_viewer.pack_forget()
+    gif_label.pack_forget()
+    next_button.pack_forget()
+    # Add start button
+    start_button = tk.Button(root, text="Start", command=lambda: collect_emg_data(participant_entry.get(),
+    float(forearm_entry.get()),
+    gesture_var.get()))
+    start_button.pack()  
+
+    # add discard button
+    discard_button = tk.Button(root, text="Discard last", command=lambda: decrease_by_one(gesture_index))
+    discard_button.pack()
+
+def show_gif(gesture, next_button, gif_label, pair=False):
+    global gif_viewer
+    gif_label.config(text = gesture)
+    next_button.destroy()
+    if gif_viewer is not None:
+        gif_viewer.destroy()
+    gif_viewer = GIFViewer(root, filename=f"media\gesture_demos\{gesture}.gif")
+    gif_viewer.pack()
+    gif_viewer.play()
+    if not pair:
+        _next_button = tk.Button(root, text="Next >>", command=lambda: clean_gif(gif_viewer, gif_label, _next_button))
+        _next_button.pack()      
+
+def show_tutorial(root, gesture):
+    global gif_label
+    pair = False
+    gif_label = tk.Label(root, font=("Helvetica", 15), pady=40)
+    gif_label.pack()
+    if gesture in gesture_pairs:
+        pair=True
+        gestures = gesture_pairs[gesture].copy()
+        gestures.pop(1)
+    else:
+        gestures = [gesture]
+    show_gif(gestures[0], next_button, gif_label, pair)
+    if pair:
+        _next_button = tk.Button(root, text="Next >>", command=lambda: show_gif(gestures[1], _next_button, gif_label))
+        _next_button.pack()
+
+# Reset GUI
+def reset_GUI(root):
+    global next_button, start_button, gif_viewer, gif_label
     for widget in root.winfo_children():
-        if isinstance(widget, tk.Entry):
-            widget.delete(0, 'end')
- 
+        if (isinstance(widget, tk.Button)):
+            widget.pack_forget()
+    if gif_viewer is not None:
+        gif_viewer.destroy()
+    if gif_label is not None:
+        gif_label.pack_forget()
+
+    if start_button is not None:
+        start_button.pack_forget()
+    if discard_button is not None:
+        discard_button.pack_forget()
+
+    # Add next button
+    next_button = tk.Button(root, text="Next >>", command=lambda: show_tutorial(root, gesture_var.get()))
+    next_button.pack()
+
+    # Add reset button
+    reset_button = tk.Button(root, text="Reset", command=lambda: reset_GUI(root))
+    reset_button.pack()
+
+
 # Create GUI
 root = tk.Tk()
 root.title("EMG Data Collection")
-root.geometry("400x300")
+root.geometry("400x600")
 
 # Add participant name input
 participant_label = tk.Label(root, text="Participant Name:")
@@ -113,16 +149,17 @@ forearm_entry.pack()
 gesture_label = tk.Label(root, text="Gesture:")
 gesture_label.pack()
 gesture_var = tk.StringVar(root)
-gesture_var.set("Neutral")
-gesture_menu = tk.OptionMenu(root, gesture_var, "Neutral", "Open Hand", "Closed Hand", "Thumb Abduction", "Thumb Adduction", "Pronation", "Supination", "Pinch", "Tripod", "Point", "Lateral")
+gesture_var.set("Select Gesture")
+gesture_menu = tk.OptionMenu(root, gesture_var, "Open-Close Hand", "Thumb Abduction-Adduction", "Wrist Pronation-Supination", "Pinch", "Tripod", "Point", "Lateral")
 gesture_menu.pack()
 
-# Add start button
-start_button = tk.Button(root, text="Start", command=lambda: collect_emg_data(participant_entry.get(),
-float(forearm_entry.get()),
-gesture_var.get(),
-0))
-start_button.pack()
+# Add next button
+next_button = tk.Button(root, text="Next >>", command=lambda: show_tutorial(root, gesture_var.get()))
+next_button.pack()
+
+# Add reset button
+reset_button = tk.Button(root, text="Reset", command=lambda: reset_GUI(root))
+reset_button.pack()
 
 #Run GUI 
 root.mainloop()
---
+++ data/utils.py
@@ -0,0 +1,107 @@
+import myo
+import numpy as np
+import time
+import os
+import tkinter as tk
+import threading
+from threading import Lock
+from PIL import Image, ImageTk
+
+
+class GIFViewer(tk.Label):
+    def __init__(self, master=None, filename=None, **kw):
+        self._gif_frames = []
+        tk.Label.__init__(self, master, **kw)
+        if filename:
+            self.load(filename)
+
+    def load(self, filename):
+        with Image.open(filename) as im:
+            for frame in range(0, im.n_frames):
+                im.seek(frame)
+                frame_image = ImageTk.PhotoImage(im)
+                self._gif_frames.append(frame_image)
+
+    def play(self):
+        animate_thread = threading.Thread(target=self._animate_gif, args = [0])
+        animate_thread.start()
+
+    def _animate_gif(self, frame):
+        self.config(image=self._gif_frames[frame])
+        frame = (frame + 1) % len(self._gif_frames)
+        self.after(40, self._animate_gif, frame)
+
+
+class EmgCollector(myo.DeviceListener):
+    def __init__(self, start, name, forearm_circumference, gesture, gesture_index, root):
+        self.name = name
+        self.start = start
+        self.forearm_circumference = forearm_circumference
+        self.gesture = gesture
+        self.gesture_index = gesture_index
+        self.root = root
+        self.emg_data = np.zeros((1, 8))
+        self.lock = Lock()
+        self.is_recording = False
+        self.countdown_label = tk.Label(root, font=("Helvetica", 15), pady=40)
+        self.countdown_label.pack()
+        self.reference_img_label = None
+
+    def on_connected(self, event):
+        event.device.stream_emg(True)
+
+    def on_emg(self, event):
+        if self.is_recording:
+            self.emg_data = np.concatenate((self.emg_data, np.reshape(np.array(event.emg), (1, 8))), axis=0)
+
+    
+    def start_recording(self, countdown_secs = 5):
+        # Start countdown timer
+        for i in range(countdown_secs):
+            self.countdown_label.config(text=f"Recording {self.gesture} in {countdown_secs-i} seconds...")
+            self.root.update()
+            time.sleep(1)
+
+        # Load and show reference gesture GIF
+        reference_img_filename = f"media/gesture_demos/{self.gesture}.png"
+        reference_img = Image.open(reference_img_filename)
+        reference_img = reference_img.resize((200, 200), Image.ANTIALIAS)
+        reference_img = ImageTk.PhotoImage(reference_img)
+        self.reference_img_label = tk.Label(self.root, image=reference_img)
+        self.reference_img_label.image = reference_img
+        self.reference_img_label.pack()
+
+        # Start recording
+        self.is_recording = True
+        self.emg_data = np.zeros((1, 8))
+        self.countdown_label.config(text=f"Recording {self.gesture}...")
+        self.root.update()
+
+    def stop_recording(self):
+        # Remove reference gif
+        if self.reference_img_label is not None:
+            self.reference_img_label.pack_forget()
+
+        # Stop recording
+        self.is_recording = False
+        self.countdown_label.config(text=f"Saving {self.gesture} data...")
+        self.root.update()
+
+        # Save EMG data to file
+        if not os.path.isdir(f"data/{self.name}"):
+            os.mkdir(f"data/{self.name}")
+
+        emg_data_array = np.array(self.emg_data)
+        filename = f"data/{self.name}/{self.gesture}_{self.gesture_index}.npy"
+        np.save(filename, emg_data_array)
+        
+        # Save forearm size 
+        filename = f"data/{self.name}/forearm_circumference.npy"
+        np.save(filename, np.array(self.forearm_circumference))
+        
+        # Increment gesture index
+        self.countdown_label.destroy()
+        self.root.update()
+
+        # Reset EMG
+        self.emg_data = np.zeros((1, 8))
